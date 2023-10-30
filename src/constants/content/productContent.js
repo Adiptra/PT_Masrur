0d2111207351c1e549c3b8b2fbff60b1236a2a04
Adiptra <adiirawan2705@gmail.com>
2023-10-30 10:32:45 +0700

    constant update

---
+++ src/constants/content/productContent.js
@@ -0,0 +1,70 @@
+export const productSection = [
+  {
+    title: "Our Products",
+    subtitle: "We have many products that we can offer",
+    gar: [
+      {
+        title: "GAR (Gross As Received) Coal",
+        lists: [
+          {
+            id: 1,
+            icon: icon,
+            desc: "GAR 6,000 – 5,800 Kcal/Kg",
+          },
+          {
+            id: 2,
+            icon: icon,
+            desc: "GAR 5,000 – 4,800 Kcal/Kg",
+          },
+          {
+            id: 3,
+            icon: icon,
+            desc: "GAR 4,200 – 4,000 Kcal/Kg",
+          },
+          {
+            id: 4,
+            icon: icon,
+            desc: "GAR 4,500 – 4,300 Kcal/Kg",
+          },
+          {
+            id: 5,
+            icon: icon,
+            desc: "GAR 5,500 – 5,300 Kcal/Kg",
+          },
+        ],
+      },
+    ],
+    nar: [
+      {
+        title: "NAR(Net As Received) Coal",
+        lists: [
+          {
+            id: 1,
+            icon: icon,
+            desc: "NAR 5,300 – 5,500 Kcal/Kg",
+          },
+          {
+            id: 2,
+            icon: icon,
+            desc: "NAR 4,500 – 4,700 Kcal/Kg",
+          },
+          {
+            id: 3,
+            icon: icon,
+            desc: "NAR 3,800 – 4,000 Kcal/Kg",
+          },
+          {
+            id: 4,
+            icon: icon,
+            desc: "NAR 4,200 – 4,400 Kcal/Kg ",
+          },
+          {
+            id: 5,
+            icon: icon,
+            desc: "NAR 4,800 – 5,000 Kcal/Kg",
+          },
+        ],
+      },
+    ],
+  },
+];
