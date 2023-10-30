2702134032b4f576ed7fd9126148580042fa4268
Adiptra <adiirawan2705@gmail.com>
2023-10-30 12:31:10 +0700

    constant update

---
+++ src/constants/content/serviceContent.js
@@ -0,0 +1,7 @@
+export const serviceSection = [
+  {
+    title: "Get our services and feel the benefits",
+    subtitle:
+      "Providing services as a transparent, fast and precise based coal commodity trading company. And Always open opportunities (improvise) to partner and invest together .",
+  },
+];
