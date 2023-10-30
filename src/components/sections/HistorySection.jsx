68032a84b558e4a22afd0966cdaa104555b92f32
Adiptra <adiirawan2705@gmail.com>
2023-10-30 16:11:59 +0700

    History Section

--- src/components/sections/HistorySection.jsx
+++ src/components/sections/HistorySection.jsx
@@ -1,14 +1,31 @@
+/* eslint-disable react/jsx-key */
+import { historySection } from "../../constants/content/historyContent";
 import { Container, Spacing } from "../../layouts";
 import React from "react";
+// import BenefitSection from "./BenefitSection";
 
 const HistorySection = () => {
+  const histories = historySection.histories;
   return (
-    <section className="w-full">
-      <Spacing>
-        <Container>
-          <h1>p</h1>
-        </Container>
-      </Spacing>
+    <section className="flex w-full text-white mt-16 bg-[#0F172A] pb-16">
+      <div className="wrapper">
+        <div className="container-1 flex justify-center items-center flex-col">
+          <div className="title text-center text-[36px] font-generalBd py-16">
+            {historySection.title}
+          </div>
+          {histories.map((history) => (
+            <div className="flex justify-between pt-4 pb-16 border-b border-[#BEC1CA] w-[70%] gap-x-[10px]">
+              <div className="left-content w-[25%] ">
+                <h1 className="font-generalSb text-[32px]">{history.year}</h1>
+                <p>{history.month}</p>
+              </div>
+              <div className="right-content w-[50%] ">
+                <h1>{history.desc}</h1>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
--- src/constants/content/historyContent.js
+++ src/constants/content/historyContent.js
@@ -1,25 +1,23 @@
-export const historySection = [
-  {
-    title: "Want to know our brief history?",
-    histories: [
-      {
-        id: 1,
-        year: "2005",
-        month: "August, 25",
-        desc: "PT. MASRUR BORNEO was founded in Banjarmasin, Indonesia.",
-      },
-      {
-        id: 2,
-        year: "2006",
-        month: "At the end",
-        desc: "The company was rebuilt. At the end of 2006, several young professionals specialized in mining and geology who have dozens of years of work experience in various well-known national mining companies.",
-      },
-      {
-        id: 3,
-        year: "The First Mission",
-        month: "In the past",
-        desc: "This company's mission is to build professionalism in the mining industry with valuable experience and certified high expertise, in creating a professional mining company to create profitability in the future.",
-      },
-    ],
-  },
-];
+export const historySection = {
+  title: "Want to know our brief history?",
+  histories: [
+    {
+      id: 1,
+      year: "2005",
+      month: "August, 25",
+      desc: "PT. MASRUR BORNEO was founded in Banjarmasin, Indonesia.",
+    },
+    {
+      id: 2,
+      year: "2006",
+      month: "At the end",
+      desc: "The company was rebuilt. At the end of 2006, several young professionals specialized in mining and geology who have dozens of years of work experience in various well-known national mining companies.",
+    },
+    {
+      id: 3,
+      year: "The First Mission",
+      month: "In the past",
+      desc: "This company's mission is to build professionalism in the mining industry with valuable experience and certified high expertise, in creating a professional mining company to create profitability in the future.",
+    },
+  ],
+};
