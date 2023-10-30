d7900b4aa6a3cc06d102650146aa8156e9af85c3
Adiptra <adiirawan2705@gmail.com>
2023-10-30 14:41:37 +0700

    Benefit Section responsive medium

--- src/components/sections/BenefitSection.jsx
+++ src/components/sections/BenefitSection.jsx
@@ -6,22 +6,22 @@ const BenefitSection = () => {
   const benefits = benefitContent.benefits;
   return (
     <div className="mt-16">
-      <div className="containerBenefit flex justify-between">
-        <div className="text-[#0F172A] text-[36px] font-generalSb w-[25%]">
+      <div className="containerBenefit flex-none md:flex justify-center md:justify-between">
+        <div className="text-[#0F172A] text-[36px] font-generalSb w-full md:w-[25%] md:text-left text-center">
           {benefitContent.title}
         </div>
-        <div className="right-content w-[75%]">
-          <div className="flex py-1">
+        <div className="right-content w-full mt-5 md:mt-0 md:w-[75%]">
+          <div className="flex-none md:flex py-1">
             {benefits.map((benefit) => (
               <div className="flex gap-3 py-2" key={benefit.id}>
-                <div className=" w-[100px] h-[47px]">
+                <div className="w-[80px] h-[45px]">
                   <TextIcon
                     icon={benefit.icon}
                     className="py-2 px-2 rounded-full bg-[#FFDCDD]"
                   />
                 </div>
                 <div className="desc-right">
-                  <div className="title font-generalSb text-[24px]">
+                  <div className="title font-generalSb text-[24px] ">
                     {benefit.title}
                   </div>
                   <div className="desc text-[16px] mt-4">{benefit.desc}</div>
