5377e2ba9ddb2d704f928abddbbe3e4cda7aecf7
Aven1426 <fathir.adlan@gmail.com>
2023-10-30 14:07:17 +0700

    otw about

--- src/components/reusable/TextIcon.jsx
+++ src/components/reusable/TextIcon.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const TextIcon = ({ className, text, icon}) => {
-  return <div>TextIcon</div>;
+const TextIcon = ({ className, text, icon }) => {
+  return (
+    <div>
+      <div className="img">
+        <div className="img-content flex items-center gap-x-3">
+          <img src={icon} alt="" className="max-w-[50px] max-h-[50px]" />
+          <p className="text-[28px] font-generalSb">{text}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TextIcon;
--- src/images/index.js
+++ src/images/index.js
@@ -2,5 +2,7 @@ import heroBanner from "./hero-banner.png";
 import exp1 from "./exp-img1.png";
 import exp2 from "./exp-img2.png";
 import serviceBanner from "./service-banner.png";
+import about from "./about-icon.svg";
+import quote from "./single-quote.svg";
 
-export { heroBanner, exp1, exp2, serviceBanner };
+export { heroBanner, exp1, exp2, serviceBanner, about, quote };
