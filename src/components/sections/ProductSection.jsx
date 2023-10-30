5b871d0b88ee68b8850b42a481833bd370ebcec5
Aven1426 <fathir.adlan@gmail.com>
2023-10-30 16:59:59 +0700

    Product updated

--- src/components/sections/ProductSection.jsx
+++ src/components/sections/ProductSection.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+import React from "react";
+import { productSection } from "../../constants/content/productContent";
 
 const ProductSection = () => {
   return (
-    <div>ProductSection</div>
-  )
-}
+    <div>
+      {productSection.map((data) => (
+        <div className="wrapper">
+          <h1>{data.title}</h1>
+          <h3>{data.subtitle}</h3>
+          <div className="type grid grid-cols-2">
+            {data.content.map((dataContent) => (
+              <div>
+                <h1>{dataContent.title}</h1>
+                {dataContent.lists.map((listContent) => (
+                  <ul className="gap-x-2 ">
+                    <li key={listContent.id} className="border-b-2 border-black">
+                      <img src={listContent.icon} alt="" />
+                      <p></p>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default ProductSection
+export default ProductSection;
--- src/constants/content/productContent.js
+++ src/constants/content/productContent.js
@@ -1,66 +1,67 @@
+import { star } from "../../images";
+
 export const productSection = [
   {
     title: "Our Products",
     subtitle: "We have many products that we can offer",
-    gar: [
+    content: [
       {
         title: "GAR (Gross As Received) Coal",
         lists: [
           {
             id: 1,
-            icon: icon,
+            icon: star,
             desc: "GAR 6,000 – 5,800 Kcal/Kg",
           },
           {
             id: 2,
-            icon: icon,
+            icon: star,
             desc: "GAR 5,000 – 4,800 Kcal/Kg",
           },
           {
             id: 3,
-            icon: icon,
+            icon: star,
             desc: "GAR 4,200 – 4,000 Kcal/Kg",
           },
           {
             id: 4,
-            icon: icon,
+            icon: star,
             desc: "GAR 4,500 – 4,300 Kcal/Kg",
           },
           {
             id: 5,
-            icon: icon,
+            icon: star,
             desc: "GAR 5,500 – 5,300 Kcal/Kg",
           },
         ],
       },
-    ],
-    nar: [
+
       {
         title: "NAR(Net As Received) Coal",
         lists: [
           {
             id: 1,
-            icon: icon,
+            icon: star,
             desc: "NAR 5,300 – 5,500 Kcal/Kg",
           },
           {
             id: 2,
-            icon: icon,
+            icon: star,
             desc: "NAR 4,500 – 4,700 Kcal/Kg",
           },
           {
             id: 3,
-            icon: icon,
+            icon: star,
             desc: "NAR 3,800 – 4,000 Kcal/Kg",
           },
           {
             id: 4,
-            icon: icon,
+            icon: star,
             desc: "NAR 4,200 – 4,400 Kcal/Kg ",
           },
           {
             id: 5,
-            icon: icon,
+            icon: star,
             desc: "NAR 4,800 – 5,000 Kcal/Kg",
           },
         ],
