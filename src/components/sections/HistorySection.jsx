62638c8cf6e17e10a8e58454236232b40239f3a7
Adiptra <adiirawan2705@gmail.com>
2023-10-30 16:33:59 +0700

    History Section

--- src/components/sections/HistorySection.jsx
+++ src/components/sections/HistorySection.jsx
@@ -12,13 +12,13 @@ const HistorySection = () => {
     <section className="flex w-full text-white mt-16 bg-[#0F172A] pb-16">
       <div className="wrapper">
         <div className="container-1 flex justify-center items-center flex-col mb-32">
-          <div className="title text-center text-[36px] font-generalBd py-16">
+          <div className="title text-center text-[36px] font-InterBd py-16">
             {historySection.title}
           </div>
           {histories.map((history) => (
             <div className="flex justify-between pt-4 pb-16 border-b border-[#BEC1CA] w-[70%] gap-x-[10px]">
               <div className="left-content w-[25%] ">
-                <h1 className="font-generalSb text-[32px]">{history.year}</h1>
+                <h1 className="font-InterBd text-[32px]">{history.year}</h1>
                 <p>{history.month}</p>
               </div>
               <div className="right-content w-[50%] ">
@@ -31,7 +31,7 @@ const HistorySection = () => {
           <TextIcon icon={iconArrow} className="" />
         </div>
         <div className="flex justify-center w-full mt-5">
-          <h1 className="text-[20px] font-generalSb">Keep Scrolling</h1>
+          <h1 className="text-[20px] font-InterBd">Keep Scrolling</h1>
         </div>
       </div>
     </section>
